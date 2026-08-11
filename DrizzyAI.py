abb805c9dbbd5ae06b2580c46b47b57d016b0883
Sakarto <avyukthmalapaka@gmail.com>
2026-08-11 05:12:08 -0700

    Add files via upload

---
+++ DrizzyAI.py
@@ -0,0 +1,42 @@
+from google import genai
+from google.genai import types
+import torch
+import torch.nn.functional as F
+import json
+from dotenv import load_dotenv 
+import os
+import streamlit as st
+
+load_dotenv()
+gemini_api_key = os.environ['GEMINI_API_KEY']
+
+
+st.title('DrizzyAI: Find a Drake song for any feeling!')
+#----------------------------------------------------------------------------------------------------------------------------------
+
+# User Input/Embedding:
+
+client = genai.Client(api_key=gemini_api_key)
+
+user_response = st.text_input("What best describes how you are feeling right now?", "Type here...")
+embedded_user_response = client.models._embed_content(model="gemini-embedding-001", contents=user_response)
+#----------------------------------------------------------------------------------------------------------------------------------
+
+# Compare Embedding and Track Selection:
+
+with open('embedded_tracks.json', 'r') as file:
+    embtracks = json.load(file)
+
+user_tensor = torch.tensor(embedded_user_response.embeddings[0].values).unsqueeze(0)
+
+
+similarities = []
+for track, embedding in embtracks.items():
+    track_tensor = torch.tensor(embedding).unsqueeze(0)
+    similarities.append((F.cosine_similarity(user_tensor, track_tensor)).item())
+
+highest_similarity_spot = similarities.index(max(similarities))
+
+if st.button("Submit"):
+    st.success("Try listening to " + list(embtracks.keys())[highest_similarity_spot] + ".")
+#----------------------------------------------------------------------------------------------------------------------------------
