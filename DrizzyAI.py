889fca9a78b7f3625958df66161f4324ee4a3558
Sakarto <avyukthmalapaka@gmail.com>
2026-09-13 05:16:11 -0700

    Tweak subheader copy in DrizzyAI
    Updates the Streamlit subheader text to add a friendlier tone with a smiley and split the message across two lines for clearer presentation.

--- DrizzyAI.py
+++ DrizzyAI.py
@@ -11,7 +11,8 @@ load_dotenv()
 gemini_api_key = os.environ['GEMINI_API_KEY']
 
 st.title('DrakeAI: Find a Drake song for any feeling!')
-st.subheader('Keep an open mind and take the advice. DrakeAI will get more accurate as songs get added.')
+st.subheader('Keep an open mind and take the advice :) \n' 
+' DrakeAI will get more accurate as songs get added.')
 #----------------------------------------------------------------------------------------------------------------------------------
 
 # User Input/Embedding:
