407d0da2386a6055ea161efaa7ac4567dc5454ae
Sakarto <avyukthmalapaka@gmail.com>
2026-09-15 00:18:40 -0700

    Fixed text alignment instead of wrapping

--- DrizzyAI.py
+++ DrizzyAI.py
@@ -10,9 +10,9 @@ import streamlit as st
 load_dotenv()
 gemini_api_key = os.environ['GEMINI_API_KEY']
 
-st.title(body='DrakeAI: Find a Drake song for any feeling!', TextAlignment="center")
-st.subheader(body='Take the advice and give it a listen :)', wrap=False)
-st.text(body="Disclaimer: DrakeAI will get more accurate over time!", wrap=False)
+st.title(body='DrakeAI: Find a Drake song for any feeling!', text_alignment="center")
+st.subheader(body='Take the advice and give it a listen :)', text_alignment="center")
+st.text(body="Disclaimer: DrakeAI will get more accurate over time!", text_alignment="center")
 #----------------------------------------------------------------------------------------------------------------------------------
 
 # User Input/Embedding:
